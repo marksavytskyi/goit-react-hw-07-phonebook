66be158e6a844ba2bc4f031aeeee1de4419512cf
marksavytskyi <dorozhe.zolota777@gmail.com>
2023-02-28 12:32:09 -0800

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
+import Filter from './Filter';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+
+const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        width: '600px',
+        borderRadius: '5px',
+        margin: '0 auto 0 auto',
+        color: '#010101',
+        padding: '40px',
+        backgroundColor: 'white',
       }}
     >
-      React homework template
+      <h2 style={{ textAlign: 'center' }}>Phonebook</h2>
+
+      <ContactForm></ContactForm>
+
+      <h2 style={{ textAlign: 'center' }}>Contacts:</h2>
+
+      <Filter></Filter>
+
+      <ContactList></ContactList>
     </div>
   );
 };
+
+export default App;
